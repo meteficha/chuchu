f328c2aae73dfed48a7ed74e317f4730c8a8f84e
Marco Túlio Gontijo e Silva <marcotmarcot@gmail.com>
2012-08-09 08:00:59 -0300

    Add exports list.

--- Test/Chuchu.hs
+++ Test/Chuchu.hs
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-module Test.Chuchu where
+module Test.Chuchu (chuchuMain, Chuchu, ChuchuParser (..), Value) where
 
 -- base
 import Control.Applicative
