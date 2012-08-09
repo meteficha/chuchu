731aba81ac340dffc5fd360ecfbd15d3eee7f62c
Marco Túlio Gontijo e Silva <marcotmarcot@gmail.com>
2012-08-09 16:29:12 -0300

    Add haddock header.

--- Test/Chuchu/Parsec.hs
+++ Test/Chuchu/Parsec.hs
@@ -1,19 +1,14 @@
--- Copyright 2012 Marco Túlio Pimenta Gontijo <marcotmarcot@gmail.com>
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 {-# OPTIONS_GHC -w #-}
--- | This is from the where clause of makeTokenParser with types included.
+-- |
+-- Module      :  Test.Chuchu.Parsec
+-- Copyright   :  (c) Marco Túlio Pimenta Gontijo <marcotmarcot@gmail.com> 2012
+-- License     :  Apache 2.0 (see the file LICENSE)
+--
+-- Maintainer  :  Marco Túlio Pimenta Gontijo <marcotmarcot@gmail.com>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- This is from the where clause of 'makeTokenParser' with types included.
 module Test.Chuchu.Parsec (natFloat, module Test.Chuchu.Types) where
 
 -- base
